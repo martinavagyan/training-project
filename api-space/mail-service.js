ce7ef4bfe468fc3587b56b9e6062d251af5afa8e
martinavagyan <martinavagyan@users.noreply.github.com>
2017-10-04 16:06:37 +0200

    js docs added to mail module

--- api-space/mail-service.js
+++ api-space/mail-service.js
@@ -1,7 +1,24 @@
 "use strict";
+/**
+ * Usage, improt the module anywhere you need to send email,
+ * @example import MailService from '../../mail-service';
+ *
+*/
 Object.defineProperty(exports, "__esModule", { value: true });
 var nodemailer = require('nodemailer');
 class MailService {
+    /**
+     * Usage:
+     * sendMail is static, thus no initialization of the class is required
+     * @param, {Object} mailOptions is an object of sender's detials
+     * @example let mailOptions = {
+     *               from: 'working.space.inc@gmail.com',
+     *               to: 'martinavagyan@gmail.com',
+     *               subject: 'Sending Email using Node.js',
+     *               text: 'That was easy!'
+     *           };
+     *
+    */
     static sendMail(mailOptions) {
         this.transporter.sendMail(mailOptions, function (error, info) {
             if (error) {
--- api-space/mail-service.ts
+++ api-space/mail-service.ts
@@ -1,7 +1,12 @@
+/**
+ * Usage, improt the module anywhere you need to send email, 
+ * @example import MailService from '../../mail-service';
+ * 
+*/
+
 var nodemailer = require('nodemailer');
 
 export default class MailService{
-
 
     static transporter:any =nodemailer.createTransport({
         service: 'gmail',
@@ -10,8 +15,19 @@ export default class MailService{
             pass: 'onepie2many'
         }
     });
-    
 
+    /**
+     * Usage:
+     * sendMail is static, thus no initialization of the class is required
+     * @param, {Object} mailOptions is an object of sender's detials
+     * @example let mailOptions = {
+     *               from: 'working.space.inc@gmail.com',
+     *               to: 'martinavagyan@gmail.com',
+     *               subject: 'Sending Email using Node.js',
+     *               text: 'That was easy!'
+     *           };
+     * 
+    */
     static sendMail(mailOptions:Object){
         this.transporter.sendMail(mailOptions, function(error:any, info:any){
             if (error) {
