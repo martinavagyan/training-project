1149f66ee8c4d2d90cc6c14f66262d8f44c4e651
Lars Steenhuis <Lars.Steenhuis-CIC.Netherlands@ibm.com>
2017-10-05 10:49:00 +0200

    beta admin panel

--- api-space/api/routes/apiRoutes.js
+++ api-space/api/routes/apiRoutes.js
@@ -9,6 +9,8 @@ module.exports = function (app) {
     app.route('/api/get-all-visits').get(apiController.get_all_visits);
     app.route('/api/get-all-hosts').get(apiController.get_all_hosts);
     app.route('/api/register-host').post(apiController.register_a_host);
+    app.route('/api/remove-all-hosts').post(apiController.remove_all_host);
+    app.route('/api/remove-all-visits').post(apiController.remove_all_visits);
     //app.route('/api/get-host/:hostId').get(apiController.get_host);
     //One test endpoint
     app.route('/api/test/').get(apiController.send_email);
